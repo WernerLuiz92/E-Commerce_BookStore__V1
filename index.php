1a4cfe2c23dafa1aa51bcd3690aaf04418f3f1d3
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-19 23:11:05 -0300

    Initial Commit
    Setup do esqueleto básico do site.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- Code -->
+<?php
+
+    include '_conexao.php';
+
+?>
+<!-- Code End -->
+
+<!-- Head -->
+<?php
+    include 'head.php';
+    $page = #;
+    include 'navbar.php'; 
+?>
+<!-- Head End -->
+
+<!-- Body -->
+<div>
+<h1>Site E-Commerce Werner!!!</h1>
+</div>
+<!-- Body End -->
+
+<!-- Footer -->
+<?php
+    include 'footer.php';
+?>
+<!-- Footer End -->
