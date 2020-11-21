862d6fbd3fbdc9bfc066e61f62baa0893a424956
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-21 02:51:46 -0300

    Barras de Navegação
    Criação de uma nova NavBar para substituir o layout antigo.
    
    - Topbar que é oculta ao rolar a pagina
    - Topbar com Brand, campo de busca e form para login
    - Navbar sticky que fica no topo da pagina, sendo um menu de acesso rápido ao usuário.

--- navbar.php
+++ navbar.php
@@ -1,69 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">
-        <img src="images/logo_site_werner.png" width="30" height="30" class="d-inline-block align-top" alt=""
-            loading="lazy">
-        Store
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?php if ($page == 1){echo 'active';} ?>">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle <?php if ($page == 2 || $page == 3 || $page == 4){echo 'active';} ?>"
-                    href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    Produtos
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item <?php if ($page == 2){echo 'active';} ?>" href="#">Cervejas</a>
-                    <a class="dropdown-item <?php if ($page == 3){echo 'active';} ?>" href="#">Canecas</a>
-                    <!--<div class="dropdown-divider"></div>-->
-                    <a class="dropdown-item <?php if ($page == 4){echo 'active';} ?>" href="#">Acessórios</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle <?php if ($page == 5 || $page == 6 || $page == 7){echo 'active';} ?>"
-                    href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    Listar Cadastros
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item <?php if ($page == 5){echo 'active';} ?>"
-                        href="listar_produto.php">Produtos</a>
-                    <a class="dropdown-item <?php if ($page == 6){echo 'active';} ?>"
-                        href="listar_categoria.php">Categorias</a>
-                    <!--<div class="dropdown-divider"></div>-->
-                    <a class="dropdown-item <?php if ($page == 7){echo 'active';} ?>"
-                        href="listar_fornecedor.php">Fornecedores</a>
-                </div>
-            </li>
-            <li class="nav-item <?php if ($page == 8){echo 'active';} ?>">
-                <a class="nav-link" href="aboutme.php">Sobre
-                    nós!</a>
-            </li>
-        </ul>
-        <form class="form-inline" action="_login.php" method="post">
-            <div class="input-group" style="width: 150px;">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="user"><i class="fas fa-user-alt"></i></span>
-                </div>
-                <input type="text" class="form-control" name="user" placeholder="Usuário" aria-label="Username"
-                    aria-describedby="user">
-            </div>
-            <div class="input-group" style="margin-left: 5px; width: 150px;">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="pass"><i class="fas fa-unlock-alt"></i></span>
-                </div>
-                <input type="password" class="form-control" name="pass" placeholder="Senha" aria-label="Username"
-                    aria-describedby="pass">
-            </div>
-            <button class="btn btn-sm btn-outline-secondary" type="submit" style="margin-left: 5px;">Login</button>
-        </form>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light border-bottom" style="background-color: #e1e1e1;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="index.php">Home</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Gêneros
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <a class="nav-link" href="#">Promoções</a>
+      <a class="nav-link" href="#">Sobre nós!</a>
     </div>
+  </div>
 </nav>
---
+++ topbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php" style="font-size: 32px;">
+        <img src="images/logo.png" width="45" height="45" class="d-inline-block align-top" alt="" loading="lazy">
+        Book Store
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline" style="margin-right: 35px; margin-left: 5px;">
+            <input class="form-control mr-sm-2" type="search" placeholder="O que você procura?" aria-label="Search" style="width: 500px;">
+            <button class="btn btn-sm btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i> Buscar</button>
+        </form>
+        <form class="form-inline" action="_login.php" method="post" style="position: absolute; right: 10px;">
+            <div class="input-group" style="width: 150px;">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="user"><i class="fas fa-user-alt"></i></span>
+                </div>
+                <input type="text" class="form-control" name="user" placeholder="Usuário" aria-label="Username"
+                    aria-describedby="user">
+            </div>
+            <div class="input-group" style="margin-left: 5px; width: 150px;">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="pass"><i class="fas fa-unlock-alt"></i></span>
+                </div>
+                <input type="password" class="form-control" name="pass" placeholder="Senha" aria-label="Username"
+                    aria-describedby="pass">
+            </div>
+            <button class="btn btn-sm btn-outline-secondary" type="submit" style="margin-left: 5px;">Login</button>
+        </form>
+    </div>
+</nav>
