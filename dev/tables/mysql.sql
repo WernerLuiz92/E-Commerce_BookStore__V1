b0ebd5e0971537188bd40f4045ebe5e36280eeb2
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-12-06 03:18:00 -0300

    Testes

--- dev/tables/mysql.sql
+++ dev/tables/mysql.sql
@@ -47,7 +47,8 @@ ALTER TABLE `book_store`.`genre_book`
     ADD CONSTRAINT `genre_book_genre`
         FOREIGN KEY (`id_gen`)
         REFERENCES `book_store`.`genre` (`id_gen`)
-            ON DELETE NO ACTION;
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE;
 
 ALTER TABLE `book_store`.`genre_book` 
     ADD INDEX `genre_book_book_idx` (`id_bk` ASC) VISIBLE;
@@ -56,7 +57,8 @@ ALTER TABLE `book_store`.`genre_book`
     ADD CONSTRAINT `genre_book_book`
         FOREIGN KEY (`id_bk`)
         REFERENCES `book_store`.`book` (`id_bk`)
-            ON DELETE NO ACTION;
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE;
 
 ALTER TABLE `book_store`.`book` 
     ADD INDEX `book_author_idx` (`id_aut_bk` ASC) VISIBLE;
@@ -65,4 +67,5 @@ ALTER TABLE `book_store`.`book`
     ADD CONSTRAINT `book_author`
         FOREIGN KEY (`id_aut_bk`)
         REFERENCES `book_store`.`author` (`id_aut`)
-            ON DELETE NO ACTION;
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE;
