b2dcac6ee531fd26965bdf0b4c10071f8e9d1c81
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-12-06 20:40:19 -0300

    Footer temporario da página

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+<div class="container">
+    <nav class="navbar fixed-bottom navbar-light bg-light">
+        <div class="row row-cols-3">
+            <div class="col">Contato:</div>
+            <div class="col">Copyright</div>
+            <div class="col">Desenvolvedor:</div>
+            <div class="col">
+                <a class="btn btn-sm btn-outline-secondary" href="dev/tables/dataimport.php" role="button">Importar
+                    dados</a>
+            </div>
+        </div>
+    </nav>
+</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
     integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
