47445d7ed7f5538108b7a6851d61c2d8dd6776f4
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-12-08 23:31:14 -0300

    Adequação ao MDB

--- footer.php
+++ footer.php
@@ -21,13 +21,18 @@
 </footer>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-</script>
+<!-- jQuery -->
+<script type="text/javascript" src="<?php echo $relpath;?>MDB/js/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="<?php echo $relpath;?>MDB/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="<?php echo $relpath;?>MDB/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="<?php echo $relpath;?>MDB/js/mdb.min.js"></script>
+
+<!-- Scripts -->
+<script type="text/javascript"></script>
+
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-  <head>
+<html lang="pt-BR">
+
+<head>
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
     <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/8b054ccba1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?php echo $relpath;?>MDB/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="<?php echo $relpath;?>MDB/css/mdb.min.css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="<?php echo $relpath;?>css/style.css">
 
     <!-- Título e FavIcon -->
     <title>E-Commerce</title>
-    <link rel="shortcut icon" href="images/logo.png" />
+    <link rel="shortcut icon" href="<?php echo $relpath;?>img/logo.png" />
 
 </head>
 
